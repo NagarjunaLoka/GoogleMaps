10ca261689dcb6a2a37efc93fb770991faaa8f43
nagarjuna <knagarjun43@gmail.com>
2019-10-31 18:49:20 +0530

    mvvm commit

--- app/src/main/java/com/example/myapplication/Auth/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Auth/MainActivity.java
@@ -1,14 +1,16 @@
-package com.example.myapplication;
+package com.example.myapplication.Auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Nagarjuna extends AppCompatActivity {
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nagarjuna);
+        setContentView(R.layout.activity_main);
     }
 }
